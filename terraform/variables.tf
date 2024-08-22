1654bc5144ebba81300107dc8d8075340bb0bed6
orsankalp <orsankalp@gmail.com>
2024-08-22 20:53:47 +0200

    test 7

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,7 +5,7 @@ variable "region" {
 }
 
 variable "clusterName" {
-  description = "Name of  the EKS cluster"
+  description = "Name of thee EKS cluster"
   type        = string
   default     = "vprofile-eks"
 }
